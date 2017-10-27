4e0a39c24c1e61b05f12df3b1ba8da2955c6d99a
Muhammad Umer Farooq <mufarooq3@gmail.com>
2017-10-27 18:53:11 +0500

    testing 7

--- app/Http/Controllers/MqttController.php
+++ app/Http/Controllers/MqttController.php
@@ -439,16 +439,12 @@ class MqttController extends Controller
 
     public function emqhook(Request $request){
         //dummy insertion for testing
-        $r=str_replace("\"{","{",$request->getContent());
-        $r=str_replace("}\"","}",$r);
+        $r=str_replace(":\"{","{",$request->getContent());
+        $r=str_replace("}\"","},",$r);
         $r=str_replace(" \"","\"",$r);
         DB::select("insert into test(`web_hook`) values('".$r."')");
-        $r = json_decode("$r");
+        $r = json_decode($r);
         DB::select("insert into test(`web_hook`) values('".json_last_error()."')");
-
-        $r = json_decode("{\"action\":\"message_publish\",\"from_client_id\":\"\",\"from_username\":\"\",\"topic\":\"greeting\",\"qos\":2,\"retain\":false,\"payload\":{\"title\":\"hook2\",\"msg\":\"zzzzzzzzzzzzzzz\",\"type\":1,\"message_id\":31},\"ts\":1509112079}");
-        DB::select("insert into test(`web_hook`) values('".json_last_error()."')");
-
 
         if($r->action == "client_connected"){
             $this->client_connected($r);

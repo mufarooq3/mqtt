a871d86f917da328bb77d0a4794d2e379284140b
Muhammad Umer Farooq <mufarooq3@gmail.com>
2017-10-27 18:55:10 +0500

    testing 12

--- app/Http/Controllers/MqttController.php
+++ app/Http/Controllers/MqttController.php
@@ -439,8 +439,8 @@ class MqttController extends Controller
 
     public function emqhook(Request $request){
         //dummy insertion for testing
-        $r=str_replace(":\"{","{",$request->getContent());
-        $r=str_replace("}\"","},",$r);
+        $r=str_replace(":\"{",":{",$request->getContent());
+        $r=str_replace("},\"","},",$r);
         $r=str_replace(" \"","\"",$r);
         DB::select("insert into test(`web_hook`) values('".$r."')");
         $r = json_decode($r);

3c3ae783bf0ee03762287e83617efadff7cff4d6
Muhammad Umer Farooq <mufarooq3@gmail.com>
2017-10-28 13:10:54 +0500

    testing 007

--- app/Http/Controllers/MqttController.php
+++ app/Http/Controllers/MqttController.php
@@ -454,7 +454,7 @@ class MqttController extends Controller
 //        DB::select("insert into test(`web_hook`) values('".$str."')");
 //        $lo = json_decode($str);
 
-        DB::select("insert into test(`web_hook`) values('".json_last_error()."+".$r->action."')");
+        DB::select("insert into test(`web_hook`) values('".json_last_error()."+".$r."')");
 //
 //        DB::select("insert into test(`web_hook`) values('".$lo."')");
 //        DB::select("insert into test(`web_hook`) values('1')");
